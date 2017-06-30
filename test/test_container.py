d833767b230cfde0b56c8df24f78deaca0b0877a
seba-1511 <seba-1511@hotmail.com>
2017-06-30 03:55:20 -0700

    Added MultiLayerLSTM

---
+++ test/test_container.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import torch as th
+import torch.nn as nn
+from torch.autograd import Variable as V
+
+from time import time
+
+from lstm import (SlowLSTM,
+                  LSTM,
+                  GalLSTM,
+                  MoonLSTM,
+                  SemeniutaLSTM,
+                  LayerNormLSTM,
+                  LayerNormGalLSTM,
+                  LayerNormMoonLSTM,
+                  LayerNormSemeniutaLSTM,
+                  MultiLayerLSTM,
+                  )
+
+
+SIZE = 16
+if __name__ == '__main__':
+    lstm = MultiLayerLSTM(SIZE, LSTM)
+    l = LSTM(16, 32)
+    h = (V(th.rand(1, 1, 32)), V(th.rand(1, 1, 32)))
+
+    x = V(th.rand(1, 1, SIZE))
+    hiddens = lstm.create_hiddens()
+
+    out, h = x, hiddens
+    for i in range(10):
+        out, h = lstm(x, h)
